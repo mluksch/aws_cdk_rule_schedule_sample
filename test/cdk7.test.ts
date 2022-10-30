7687b472ec001b2c6d692648cd274916c0995011
mluksch <minimuslampe@gmail.com>
2022-10-30 10:56:42 +0100

    Initial commit

---
+++ test/cdk7.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cdk7 from '../lib/cdk7-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk7-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cdk7.Cdk7Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
