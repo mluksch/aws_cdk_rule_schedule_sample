4944501b11c3bc566fb3bd802feed30fbf632b81
mluksch <minimuslampe@gmail.com>
2022-11-03 23:56:34 +0100

    test ci pipeline

--- lib/AppStage.ts
+++ lib/AppStage.ts
@@ -4,13 +4,7 @@ import { Cdk7Stack } from "./cdk7-stack";
 
 export class AppStage extends Stage {
   constructor(parent: Construct, env: "staging" | "production") {
-    super(parent, env, {
-      outdir: "cdk.out",
-      env: {
-        account: "536209942758",
-        region: "eu-central-1",
-      },
-    });
-    new Cdk7Stack(this, `schedule-test-stack`, env);
+    super(parent, env);
+    const stack = new Cdk7Stack(this, `schedule-test-stack`, env);
   }
 }
