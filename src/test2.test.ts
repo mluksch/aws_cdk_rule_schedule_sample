0b9202d3fcdcf9b84f64dc5f02746a0110d854a1
mluksch <minimuslampe@gmail.com>
2022-11-05 13:55:52 +0100

    Revert "added bug to test"
    This reverts commit f89ca7121fefbffa1f35a3228f45a2699fb76a2b.

--- src/test2.test.ts
+++ src/test2.test.ts
@@ -2,6 +2,6 @@ import { generateBody } from "./test2";
 
 describe("Test test2", () => {
   it("can run", async () => {
-    expect(["Schere1", "Stein1", "Papier1"]).toContain(generateBody());
+    expect(["Schere", "Stein", "Papier"]).toContain(generateBody());
   });
 });
